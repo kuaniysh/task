4f60bcea17f95965fb8a25a8056de0a5a5464ffb
kuanysh <kuanysh.bekturganov@aitudala.kz>
2020-11-03 10:00:24 +0600

    release v2

--- src/main/kotlin/com/task/beeline/controller/TaskController.kt
+++ src/main/kotlin/com/task/beeline/controller/TaskController.kt
@@ -4,41 +4,28 @@ import com.task.beeline.model.Task
 import com.task.beeline.service.TaskService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/v1/api/tasks")
 class TaskController(private val taskService: TaskService) { // Внедряем наш сервис в качестве зависимости
+
     @GetMapping
-    fun getTasks(model: Model): String {
-        val tasks: List<Task> = taskService.getTasks() // И возвращает результат метода all нашего сервиса. Функциональная запись с выводом типа
-        model.addAttribute("tasks", tasks)
-        return "task-list"
-    }
+    fun getTasks(): List<Task> = taskService.getTasks() // И возвращает результат метода all нашего сервиса. Функциональная запись с выводом типа
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED) // Указываем специфический HttpStatus при успешном ответе
-    fun addTask(@RequestBody task: Task): String {
-        taskService.addTask(task)
-        return "redirect:/tasks"
-    }
+    fun addTask(@RequestBody task: Task): ResponseEntity<Task> = taskService.addTask(task)
 
     @GetMapping("/{id}")
-    fun getTaskById(@PathVariable(value = "id") taskId: Long): ResponseEntity<Task> =
-            taskService.getTaskById(taskId)
+    fun getTaskById(@PathVariable(value = "id") taskId: Long): ResponseEntity<Task> = taskService.getTaskById(taskId)
 
     @PutMapping("/{id}")
     fun updateTaskById(
             @PathVariable(value = "id") taskId: Long,
-            @RequestBody newTask: Task): String {
-        taskService.putTask(taskId, newTask)
-        return "redirect:/tasks"
-    }
+            @RequestBody newTask: Task): ResponseEntity<Task> =
+            taskService.putTask(taskId, newTask)
 
     @GetMapping("task-delete/{id}")
-    fun deleteTask(@PathVariable("id") taskId: Long): String {
-        taskService.deleteTask(taskId)
-        return "redirect:/tasks"
-    }
+    fun deleteTask(@PathVariable("id") taskId: Long): ResponseEntity<Void> = taskService.deleteTask(taskId)
 }
